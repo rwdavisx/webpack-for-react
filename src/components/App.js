387336028d6f2f207559b96a65caff6e9d85d0ae
Ryan Davis <rwdavisx@gmail.com>
2018-06-12 15:21:15 -0400

    Code Splitting

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import importedComponent from 'react-imported-component';
 
 import Home from './Home';
-import DynamicPage from './DynamicPage';
-import NoMatch from './NoMatch';
+import Loading from './Loading';
 
+const AsyncDynamicPage = importedComponent(() => import('./DynamicPage'), {
+  LoadingComponent: Loading
+});
+const AsyncNoMatch = importedComponent(() => import('./NoMatch'), {
+  LoadingComponent: Loading
+});
 const App = () => {
   return (
     <Router>
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/dynamic" component={DynamicPage} />
-          <Route component={NoMatch} />
+          <Route exact path="/dynamic" component={AsyncDynamicPage} />
+          <Route component={AsyncNoMatch} />
         </Switch>
       </div>
     </Router>
